9bfac5f2ace9ab78d9dfddc4f1aa17b0dbfcef3a
Valentin Radu <valentin.radu@codewisesystems.com>
2016-11-03 15:25:03 +0200

    Initial Commit

---
+++ SvgToCoreGraphics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SvgToCoreGraphics
+//
+//  Created by Valentin Radu on 03/11/2016.
+//  Copyright © 2016 valentinradu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
